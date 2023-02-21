d4eb60f722c7a443ce832e95140e5c585a9788b5
trong <t.vu@codeur.online>
2023-02-21 16:42:00 +0100

    fix: simplifier server js

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import mysql from 'mysql';
 import bodyParser from 'body-parser';
-import { config } from "./config.js";
 
 const app = express();
 app.use(bodyParser.json());                 // support  encoded Json
@@ -9,42 +8,12 @@ app.use(bodyParser.urlencoded({             // support  encoded URL
     extended: true
 }));
 
-const db_connection = mysql.createConnection(config);           // create SQL connection
-
-db_connection.connect((err) => {                                // execute the connection
-    (err) ? console.log(err) : console.log(db_connection)
-});
-
 app.get('/', (req, res) => {
     res.send('Simple API to call un message "Hello World"');                   
 });
 
 app.get('/test', (req, res) => {                                // Api to send a data JSON , being "message"
     res.json({message: 'Test : this is the message from server'});
-});
-
-app.get('/api/getnotes', (req, res) => {
-    let sql = "SELECT * FROM posts ORDER BY created_at DESC";
-    db_connection.query(sql, (err, results) => {
-        if (err) throw err;
-        res.json({data: results});
-    })
-});
-
-app.post('/api/noteId', (req, res) => {
-    let sql = "SELECT * FROM posts WHERE id = "+req.body.id;
-    db_connection.query(sql, (err, results) => {
-        if (err) throw err;
-        res.json({data: results});
-    })
-});
-
-app.post('/api/create', (req, res) => {
-    let sql = "INSERT INTO posts(title, description) VALUES('"+req.body.title+"', '"+req.body.description+"')";
-    db_connection.query(sql, (err, results) => {
-        if (err) throw err;
-        res.json({data: results});
-    })
 });
 
 app.listen(4000, () => console.log('App listening on port 4000'));
